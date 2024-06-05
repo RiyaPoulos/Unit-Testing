0cfde10d32c755b0e8d86b9848fda451ce37b072
riya poulose <riya.poulose@diligentglobal.com>
2024-06-05 21:14:44 +0530

    module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { StudentComponent } from './student/student.component';
+import { PropertbindingComponent } from './propertbinding/propertbinding.component';
+import { EventbindingComponent } from './eventbinding/eventbinding.component';
+import { CountcheckComponent } from './countcheck/countcheck.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    PropertbindingComponent,
+    EventbindingComponent,
+    CountcheckComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+ 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
